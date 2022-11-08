539dd15277a152978b5d238e78468d3563008a97
mikmail <mikael@rund.no>
2022-11-08 13:54:55 +0100

    lagt til navbar med logout hvis logget inn og login/signup når logget ut, lagt til mulighet for edit/delete men ikke helt fullført. Scopes er også lagt til

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,4 +3,7 @@ class Event < ApplicationRecord
 
   has_many :attending_events
   has_many :attendees, through: :attending_events, source: :user
+
+  scope :future, -> {where('event_post_date > ?', Date.today)}
+  scope :past,   -> {where('event_post_date < ?', Date.today)}
 end
