fa51a64b975cb3b8f2d8741d9a80fa317330a882
mikmail <mikael@rund.no>
2022-11-09 15:16:41 +0100

    viser inviterte brukere

--- app/models/invitation.rb
+++ app/models/invitation.rb
@@ -1,6 +1,5 @@
 class Invitation < ApplicationRecord
   belongs_to :user 
   belongs_to :inviter, class_name: "User"
-  validates_uniqueness_of :user_id, 
-    message: "is already going to this event"
+  validates_uniqueness_of :user_id
 end
